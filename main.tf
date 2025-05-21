7389acf4322c200e1a6703aa39445b9d6066b255
root <root@email.com>
2025-05-21 21:37:48 +0700

    added terraform

--- main.tf
+++ main.tf
@@ -24,4 +24,72 @@ resource "aws_secretsmanager_secret" "example_secret" {
 resource "aws_secretsmanager_secret_version" "example_secret_version" {
   secret_id     = aws_secretsmanager_secret.example_secret.id
   secret_string = "{\"username\":\"admin\",\"password\":\"supersecretpassword\"}"
+}
+
+resource "aws_instance" "exposed_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-12345678" # Replace with your subnet ID
+  associate_public_ip_address = true # This is a vulnerability: public IP
+
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "vpc-12345678" # Replace with your VPC ID
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ebs_volume" "unencrypted_volume" {
+  availability_zone = "us-east-1a"
+  size              = 1
+  encrypted         = false # This is a vulnerability: unencrypted volume
+}
+
+resource "aws_iam_role" "excessive_permissions" {
+  name = "excessive_permissions_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "excessive_permissions_policy" {
+  name = "excessive_permissions_policy"
+  role = aws_iam_role.excessive_permissions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "*"
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 } 
